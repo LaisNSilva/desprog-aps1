bea97205a5c8f0fc6becf6843aadb4fcda92d2f2
LaisNSilva <silva.lais72@hotmail.com>
2021-03-17 09:52:57 -0300

    tentativa verify

--- geometry.c
+++ geometry.c
@@ -2,7 +2,34 @@
 
 #include "geometry.h"
 
+
 int verify(point p, point a, point b) {
+    double m = (a.y-b.y)/(a.x - b.x);
+    double eq_reta = m*(p.x - a.x)+a.y;
+    double y_ponto = (double)p.y;
+    if (abs(y_ponto-eq_reta)<0.000001){
+        return 2;
+    }
+
+    else{
+        int x_max;
+        if(a.x >= b.x){
+            x_max = a.x;
+        }
+        else{
+            x_max = b.x;
+        }
+        for(int i = p.x; i<= x_max; i++ ){
+            double v = m*(i - a.x)+a.y;
+           if (abs(y_ponto-v)<0.000001){
+                return 1;
+            } 
+
+        }
+
+    }
+
+    
     return 0;
 }
 
