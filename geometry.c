fabacaa98a74b00cb1e1bcec19b822ab8a96c979
William Silva <william.silva.ismart@gmail.com>
2021-03-18 15:35:51 -0300

    apaguei os comentários

--- geometry.c
+++ geometry.c
@@ -33,41 +33,24 @@ int verify(point p, point a, point b) {
         return 2;
     }
 
-
-    
-    // se a.x e b.x forem iguais
     if (a.x == b.x){
-        // printf("a.x: %d\n", a.x);
-        // printf("b.x: %d\n", b.x);
-        // printf("a.y: %d\n", a.y);
-        // printf("b.y: %d\n", b.y);
-        // printf("p.x: %d\n", p.x);
-        // printf("p.y: %d\n", p.y);
-        // verifica se está na reta
         if (p.x == a.x){
-            // verifica se está nos limites do segmento
             if (p.y > y_min && p.y < y_max){
                 return 2;
             }
-            // se não está no limite do segmento o raio disparado não cruza
             else{
                 return 0;
             }
         }
-        // se não esta no mesmo x
         else{
-            // se estiver nos limites de y o raio cruza
-            //apenas de p.x estiver a esquerda da linha
             if(p.x < a.x){
                 if (p.y > y_min && p.y <= y_max){
                     return 1;
                 }
-                // se esta fora dos limites o raio não curza
                 else{
                     return 0;
                 }
             }
-            // se p.x estiver a direita da linha não cruza
             else{
                 return 0;
             }
@@ -75,29 +58,21 @@ int verify(point p, point a, point b) {
         }
     }
 
-    // se a.y e b.y forem iguais
     else if (a.y == b.y){
-        // ve esta está na reta
         if (p.y == a.y){
-            // ve esta no segmento
             if (p.x >= x_min && p.x<=x_max){
                 return 2;
             }
-            // se não estiver no segmento o raio cruza toda a aresta
             else{
                 return 0;
             }
         }
-        // se não tem o mesmo y o raio a direita não cruza essa aresta
         else{
             return 0;
         }
     }
 
-
-
     else{
-        // sem divisões por zero
         double m = (double)(a.y-b.y)/ (double)(a.x - b.x);
         double eq_reta = (double) m*(p.x - a.x)+a.y;
         double y_ponto = (double)p.y;
@@ -105,37 +80,15 @@ int verify(point p, point a, point b) {
         if (verifica < 0){
             verifica = eq_reta-y_ponto;
         }
-        printf("a.x: %d\n", a.x);
-        printf("a.y: %d\n", a.y);
-        printf("b.x: %d\n", b.x);
-        printf("b.y: %d\n", b.y);
-        printf("p.x: %d\n", p.x);
-        printf("p.y: %d\n", p.y);
-        printf("m: %f\n", m);
-        printf("verifica: %f\n", verifica);
-        printf("y_ponto: %f\n", y_ponto);
-        printf("eq_reta: %f\n", eq_reta);
-        
-        //printf("y_ponto = %f", y_ponto);
         if (verifica < 0.000001){
-            // printf("%f\n", verifica);
-            // printf("%d\n", p.y);
-            // printf("%d\n", y_min);
-            // printf("%d\n", y_max);
-            
-            // está contido da reta, mas não sei ainda se está fora dos limites do segmento
-            // se está contido no segmento
             if (p.y > y_min && p.y < y_max){
                 return 2;
             }
-            // se está na reta mas não no segmento, esta para fora
             else{
                 return 0;
             }
             
         }
-
-        // não está sobre a reta
         else {
             if (p.y > y_min && p.y <= y_max){
                 if(p.x <= x_min){
@@ -144,7 +97,6 @@ int verify(point p, point a, point b) {
                     double x_ponto = (double) p.x;
                     if(p.x < x_max){
                         for(double i = x_ponto; i <= x_max; i += 0.000001){
-                            // descobre o y da reta para cada x do raio 
                             double v = (double) m*(i - a.x)+a.y;
                             double verifica2 = (double) y_ponto-v;
                             if (verifica2 < 0){
@@ -162,9 +114,7 @@ int verify(point p, point a, point b) {
                 return 0;
             }
         }
-
     }
-    // printf("entrei aqui 5\n");
     return 0;
 }
 
